26e9e275ec7b9337d471959fc4f8f596d206c285
boy6728 <boy6728@naver.com>
2020-08-30 18:48:47 +0900

    Developed complete calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -21,8 +21,8 @@ const TableHeader = () => {
             <tr>
                 {
                     init.day.map(
-                        ( item ) => {
-                            return <td>{item}</td>
+                        ( item, index ) => {
+                            return <td key={index}>{item}</td>
                         }
                     )
                 }
@@ -34,8 +34,8 @@ const TableHeader = () => {
 const CalendarDays = ( props ) => {
     return (
         props.items.map(
-            ( item ) => {
-                return <td>{item === 0 ? '' : item}</td>
+            ( item, index ) => {
+                return <td key={index}>{item === 0 ? '' : item}</td>
             }
         )
     )
@@ -44,9 +44,9 @@ const CalendarDays = ( props ) => {
 const CalendarWeeks = ( props ) => {
     return (
         props.rows.map(
-            ( row ) => {
+            ( row, index ) => {
                 return (
-                    <tr>
+                    <tr key={index}>
                         <CalendarDays items={row} />
                     </tr>
                 )
@@ -57,37 +57,50 @@ const CalendarWeeks = ( props ) => {
 
 const TableBody = () => {
 
-    let first_day = new Date( init.today.getFullYear(), init.today.getMonth(), 1 ),
-        last_day = new Date( init.today.getFullYear(), init.today.getMonth(), 0 )
+    const calendarData = []
 
-    const calendarData = [ new Array() ]
+    let firstDate = new Date( init.today.getFullYear(), init.today.getMonth(), 1 ),
+        lastDate = new Date( init.today.getFullYear(), init.today.getMonth() + 1, 0 )
+
+    let calendar_total_date = firstDate.getDay() + lastDate.getDate()
+
+    calendar_total_date = calendar_total_date / 7
+    calendar_total_date = Math.ceil( calendar_total_date )
+
+    for ( let i = 0; i < calendar_total_date; i += 1 ) {
+        calendarData.push( new Array() )
+    }
 
     // Push first week to calendarData.
-    for ( let i = 0, first_week_item = 1; i <= 6; i += 1 ) {
-        if ( first_day.getDay() <= i ) {
+    for ( let i = 0, first_week_date = 1; i <= 6; i += 1 ) {
+        if ( firstDate.getDay() <= i ) {
 
-            calendarData[ 0 ].push( first_week_item )
+            calendarData[ 0 ].push( first_week_date )
 
-            first_week_item += 1
+            first_week_date += 1
         }
         else calendarData[ 0 ].push( 0 )
     }
 
-    
+    // Push date of week to calendarData.
+    for ( let i = 1; i < calendar_total_date - 1; i += 1 ) {
 
-    const rows = [
-        [0, 0, 0, 0, 0, 0, 1],
-        [2, 3, 4, 5, 6, 7, 8],
-        [9, 10, 11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20, 21, 22],
-        [23, 24, 25, 26, 27, 28, 29],
-        [30, 31, 0, 0, 0, 0, 0],
-    ]
+        let first_date_of_week = calendarData[ i - 1 ][ 6 ] + 1;
+        
+        for ( let j = first_date_of_week; j <= first_date_of_week + 6; j += 1 )
+            calendarData[ i ].push( j );
+    }
+
+    // Push date of last week to calendarData.
+    let first_date_of_last_week = lastDate.getDate() - lastDate.getDay()
+
+    for ( let i = first_date_of_last_week; i <= lastDate.getDate(); i += 1 ) {
+        calendarData[ calendar_total_date - 1 ].push( i )
+    }
 
     return (
         <tbody className="days">
-            {calendarData}
-            <CalendarWeeks rows={rows} />
+            <CalendarWeeks rows={calendarData} />
         </tbody>
     )
 }
