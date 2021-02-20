1bd20458cf914b1d6e41dd751aebdddc094e0903
boy672820 <boy672820@gmail.com>
2021-02-20 16:21:36 +0900

    First commit

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 import {
     Container,
     Table,
-    Form
+    Card,
+    Button
 } from 'react-bootstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBurn, faEdit } from "@fortawesome/free-solid-svg-icons"
 
 import { CalendarPropsInterface, CalendarStateInterface } from './calendar.interface'
 
@@ -27,29 +30,90 @@ class Calendar extends Component<CalendarPropsInterface, CalendarStateInterface>
         return (
             <main className="main">
                 <Container>
-                    <h3>2021년 2월</h3>
 
-                    <Table className="calendar-table">
-                        <thead>
-                            <tr>
-                                <th className="text align center">날짜</th>
-                                <th className="text align left">운동</th>
-                                <th className="text align center">완료</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td className="text align center" width="60">1</td>
-                                <td className="text align left"><a href="/calendar" onClick={this.handleClick}>스쿼트 / 데드리프트 / 덤벨 컬 / 클로즈그립 벤치프레스</a></td>
-                                <td className="text align center" width="60"><Form.Check type="checkbox" /></td>
-                            </tr>
-                            <tr>
-                                <td className="text align center" width="60">2</td>
-                                <td className="text align left"><a href="/calendar" onClick={this.handleClick}>기록을 남겨주세요.</a></td>
-                                <td className="text align center" width="60"><Form.Check type="checkbox" /></td>
-                            </tr>
-                        </tbody>
-                    </Table>
+                    <header className="calendar-header">
+                        <h3>2021년 2월</h3>
+                    </header>
+
+                    <Card>
+                        <Card.Body className="no padding">
+                            <Table className="no margin">
+                                <thead>
+                                    <tr>
+                                        <th className="text align center">날짜</th>
+                                        <th>내용</th>
+                                        <th className="text align center">기록</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td className="vertical text align middle center">1(월)</td>
+                                        <td className="vertical align middle"><FontAwesomeIcon icon={ faBurn } style={ { color: '#dc3545' } } />&nbsp;캔디토 상체 컨트롤데이</td>
+                                        <td className="vertical text align middle center">
+                                            <Button variant="link" className="no padding">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="vertical text align middle center">2(화)</td>
+                                        <td className="vertical align middle"><FontAwesomeIcon icon={ faBurn } />&nbsp;캔디토 하체 컨트롤데이</td>
+                                        <td className="vertical text align middle center">
+                                            <Button variant="link" className="no padding">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="vertical text align middle center">3(수)</td>
+                                        <td className="vertical align middle"><FontAwesomeIcon icon={ faBurn } />&nbsp;캔디토 하체 컨트롤데이</td>
+                                        <td className="vertical text align middle center">
+                                            <Button variant="link" className="no padding">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="vertical text align middle center">4(목)</td>
+                                        <td className="vertical align middle"><FontAwesomeIcon icon={ faBurn } />&nbsp;캔디토 하체 컨트롤데이</td>
+                                        <td className="vertical text align middle center">
+                                            <Button variant="link" className="no padding">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="vertical text align middle center">5(금)</td>
+                                        <td className="vertical align middle"><FontAwesomeIcon icon={ faBurn } />&nbsp;캔디토 하체 컨트롤데이</td>
+                                        <td className="vertical text align middle center">
+                                            <Button variant="link" className="no padding">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="vertical text align middle center">6(토)</td>
+                                        <td className="vertical align middle"><FontAwesomeIcon icon={ faBurn } />&nbsp;캔디토 하체 컨트롤데이</td>
+                                        <td className="vertical text align middle center">
+                                            <Button variant="link" className="no padding">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className="vertical text align middle center">7(일)</td>
+                                        <td className="vertical align middle"><FontAwesomeIcon icon={ faBurn } />&nbsp;캔디토 하체 컨트롤데이</td>
+                                        <td className="vertical text align middle center">
+                                            <Button variant="link" className="no padding">
+                                                <FontAwesomeIcon icon={ faEdit } />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+
                 </Container>
             </main>
         )
