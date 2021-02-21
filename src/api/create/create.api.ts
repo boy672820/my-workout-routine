910e76284f7a73c5a48a77b88afba6324d99b90f
boy672820 <boy672820@gmail.com>
2021-02-21 19:53:54 +0900

    Update create: Update api url

--- src/api/create/create.api.ts
+++ src/api/create/create.api.ts
@@ -8,7 +8,7 @@ export class CreateAPI {
     public static async createBlock( data: CreateBlockDTO ) {
         return await axios( {
             method: 'post',
-            url: '/create/block',
+            url: '/routine/block',
             data: data
         } )
     }
