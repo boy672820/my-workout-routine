5629fc2caf3f89a52fa3341ca8f099e30e421a87
boy672820 <boy672820@gmail.com>
2021-02-21 18:39:07 +0900

    Update calendar: update create type

--- src/components/calendar/calendar.interface.ts
+++ src/components/calendar/calendar.interface.ts
@@ -3,8 +3,9 @@ export interface CalendarPropsInterface {
 }
 
 export interface CalendarStateInterface {
-    modal: boolean,
-    modal_title: string,
-    routine_id: null | number,
+    modal: boolean
+    modal_title: string
+    routine_id: null | number
+    routine_date: string
     block_title: string
 }
